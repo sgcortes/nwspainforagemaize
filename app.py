cd8fcfba941564d450710be68694c5b5a473d0a4
Silverio Garcia-Cortes <sgcortes@uniovi.es>
2025-05-15 17:48:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,9 +54,9 @@ with col1:
         prec_max = df_site["Precipitation(mm)"].max()
         prec_mean = df_site["Precipitation(mm)"].mean()
 
-        rad_min = df_site["Radiation(MJ/m2 día)"].min()
-        rad_max = df_site["Radiation(MJ/m2 día)"].max()
-        rad_mean = df_site["Radiation(MJ/m2 día)"].mean()
+        rad_min = df_site["Radiation(MJ/m2day)"].min()
+        rad_max = df_site["Radiation(MJ/m2day)"].max()
+        rad_mean = df_site["Radiation(MJ/m2day)"].mean()
 
         whc_mean = df_site["WHC(mm)"].mean()
         c_mean = df_site["C(%)"].mean()
