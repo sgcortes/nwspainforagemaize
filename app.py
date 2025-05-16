087289335cd6a48a1041a63e4c8c35116fbe0076
Silverio Garcia-Cortes <sgcortes@uniovi.es>
2025-05-17 01:40:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import pandas as pd
 import joblib
 from PIL import Image
 
-#
-from PIL import Image
-
 # --- Código para verificar la versión de LightGBM ---
 try:
     lgb_version = lightgbm.__version__
@@ -157,8 +154,8 @@ with col1:
         datapredict = datapredict[expected_columns]
         #st.write("Shape del DataFrame:", datapredict.shape)
         #st.write("Columnas del DataFrame:", datapredict.columns.tolist())
-        st.session_state["pred_dm"] = round(model_dm.predict(datapredict)[0], 2)
         st.session_state["pred_ufl"] = round(model_ufl.predict(datapredict)[0], 2)
+        st.session_state["pred_dm"] = round(model_dm.predict(datapredict)[0], 2)
         st.session_state["pred_cp"] = round(model_cp.predict(datapredict)[0], 2)
 
 
